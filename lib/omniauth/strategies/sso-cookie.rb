8809181b6a782988527a4fcd19068ad923bfb016
Attila-Mihaly Balazs <dify.ltd@gmail.com>
2013-08-22 17:38:22 +0300

    Made decryption methods class level so that we can call them from outside

--- lib/omniauth/strategies/sso-cookie.rb
+++ lib/omniauth/strategies/sso-cookie.rb
@@ -17,7 +17,9 @@ module OmniAuth
       def request_phase
         auth_cookie = request.cookies[options.cookie_name]
         begin
-          auth_cookie = decrypt_cookie(auth_cookie) unless auth_cookie.nil?
+          auth_cookie = SsoCookie.
+            decrypt_cookie(auth_cookie, options.encryption_key, options.hmac_key) \
+            unless auth_cookie.nil?
         rescue
           self.log(:warn, "Error decoding '#{auth_cookie}'")
           auth_cookie = nil
@@ -31,27 +33,27 @@ module OmniAuth
 
       uid do
         auth_cookie = request.cookies[options.cookie_name]
-        data = self.decrypt_cookie(auth_cookie)
+        data = SsoCookie.decrypt_cookie(auth_cookie, options.encryption_key, options.hmac_key)
         data[options.uid_field]
       end
 
       info do
         auth_cookie = request.cookies[options.cookie_name]
-        self.decrypt_cookie(auth_cookie)
+        SsoCookie.decrypt_cookie(auth_cookie, options.encryption_key, options.hmac_key)
       end
 
-      def decrypt_cookie(cookie)
+      def SsoCookie.decrypt_cookie(cookie, encryption_key, hmac_key)
         data = Base64.strict_decode64(cookie)
         prefix, data, hmac = data[0..2], data[3..-33], data[-32..-1]
-        target_hmac  = OpenSSL::HMAC.digest('sha256', options.hmac_key, data)
-        if !constant_time_comparison(hmac, target_hmac)
+        target_hmac  = OpenSSL::HMAC.digest('sha256', hmac_key, data)
+        if !SsoCookie.constant_time_comparison(hmac, target_hmac)
           fail("Authentication error!")
         end
         if prefix == '$2$'
           iv, data = data[0..15], data[16..-1]
           cipher = OpenSSL::Cipher.new('aes-256-cbc')
           cipher.decrypt
-          cipher.key = options.encryption_key
+          cipher.key = encryption_key
           cipher.iv = iv
           decrypted = cipher.update(data) << cipher.final()
         elsif prefix == '$1$'
@@ -63,7 +65,7 @@ module OmniAuth
         result['expires'].to_i <= Time.now.to_i ? nil : result
       end
 
-      def constant_time_comparison(a, b)
+      def SsoCookie.constant_time_comparison(a, b)
         check = a.bytesize ^ b.bytesize
         a.bytes.zip(b.bytes) { |x, y| check |= x ^ y.to_i }
         return check == 0
